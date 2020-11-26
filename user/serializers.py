e864d6ac2e223813939d7fec3684b5340d006afd
tanabahh <tasyamozhanova@gmail.com>
2020-11-26 14:33:27 +0300

    division into read-only and write-only

--- user/serializers.py
+++ user/serializers.py
@@ -1,23 +1,35 @@
 from django.contrib.auth.models import User
-
 from rest_framework import serializers
 
-#from .models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
+class ReadOnlyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'username', 'password')
-    #name = serializers.CharField(max_length=255)
-    #email = serializers.EmailField()
+        fields = ('username', 'first_name', 'last_name', 'is_active',
+                  'last_login', 'is_superuser')
+        read_only_fields = ('last_login', 'is_superuser')
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class WriteOnlyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name',
+                  'password', 'is_active')
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.email = validated_data.get('email', instance.email)
         instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name)
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name)
         instance.password = validated_data.get('password', instance.password)
+        instance.is_active = validated_data.get(
+            'is_active', instance.is_active)
         instance.save()
         return instance
